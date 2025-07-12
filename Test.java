f02d9cf7adc370287b31ad9011a2c8c10e7299ee
VeNoM0011 <abhishekmahajan255@gmail.com>
2025-07-12 22:33:41 +0530

    second commit

--- Test.java
+++ Test.java
@@ -1,4 +1,4 @@
-class TestExample {
+class Test {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is a test Java program.");
