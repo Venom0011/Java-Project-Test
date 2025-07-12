00af411835eb87e7b0f64762753f1a4aa784eec4
VeNoM0011 <abhishekmahajan255@gmail.com>
2025-07-12 22:23:53 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,7 @@
+class TestExample {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a test Java program.");
+        System.out.println("Hello From Java.");
+    }
+}
